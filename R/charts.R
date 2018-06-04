94fc0ce4ac1fe2cff3865455264e4cf3ca8662f4
Martyna Kania <martynaakaniaa@gmail.com>
2018-06-04 19:44:40 +0200

    Added plot with 3 variable

--- R/charts.R
+++ R/charts.R
@@ -3,8 +3,7 @@ library("shiny")
 lr <- data.frame(read.csv('log/lr.log', header=FALSE, sep='|'))
 dccc <- data.frame(read.csv('log/dccc.log', header=FALSE, sep='|'))
 
-extractChosentLogs <- function(df){
-  df<-lr
+extractChosentLogs <- function(df, model_name){
   colnames(df) <- c(
     "df_name",
     "submodel_name",
@@ -16,18 +15,24 @@ extractChosentLogs <- function(df){
     "accuracy",
     "f1_score"
   )
-  df <- df[df$submodel_name =='rpart',]
+  df <- df[df$submodel_name == model_name,]
   df <-  df[df$nsets == 100,]
   df <-  df[df$mset == 1000,]
   df
 }
 
-rpart <- extractChosentLogs(lr);
+lr <- extractChosentLogs(lr,'rpart');
+dccc <- extractChosentLogs(dccc,'rpart');
+#dccc <- extractChosentLogs(lr, 'bagging');
 
-df = data.frame(dr$accuracy, AMK, SK, JR)
+nfeatures <- lr$nfeatures
+letter_recognition <-  lr$accuracy
+default_detection <-  dccc$accuracy
+df = data.frame(nfeatures, letter_recognition, default_detection)
 
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 
-ggplot(rpart, mapping = aes(x = nfeatures, y = accuracy) ) + geom_line()
+dfplot <- df %>% gather(key, value, -nfeatures)
+ggplot(dfplot , mapping = aes(x = nfeatures, y = value, color = key) ) + geom_line()
